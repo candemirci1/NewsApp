3e8661a043553f1ea3ac50a32be56169f0ec6d1d
can <icrimednac95@gmail.com>
2022-06-29 21:16:44 +0300

    add security

--- app/src/main/java/com/example/newsapp/data/service/NewsService.kt
+++ app/src/main/java/com/example/newsapp/data/service/NewsService.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.data.service
 
+import com.example.newsapp.BuildConfig
 import com.example.newsapp.data.model.NewsDTO
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,18 +10,18 @@ interface NewsService {
     @GET("everything")
     suspend fun fetchNews(
         @Query("q") q: String,
-        @Query("apiKey") apiKey: String = myKey
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
     ): NewsDTO
 
     @GET("top-headlines")
     suspend fun fetchCategoryNews(
         @Query("category") category: String,
-        @Query("apiKey") apiKey: String = myKey,
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
         @Query("country") country: String = "tr"
     ) : NewsDTO
 
 }
 
 
-val myKey = "991d881e0eae4dc6a80a68842c7579e1"
+
 
